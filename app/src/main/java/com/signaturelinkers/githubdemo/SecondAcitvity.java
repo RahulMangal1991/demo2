c89efccd1e111a75fd1cc1a17cdc752f3f0989ce
rahulmangal1991 <rmangal1991@gmail.com>
2017-03-26 00:55:39 +0530

    done

---
+++ app/src/main/java/com/signaturelinkers/githubdemo/SecondAcitvity.java
@@ -0,0 +1,17 @@
+package com.signaturelinkers.githubdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Rahul Mangal on 3/26/2017.
+ */
+
+public class SecondAcitvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ mydemolib/src/main/java/com/signaturelinkers/mydemolib/Rahul2.java
@@ -0,0 +1,11 @@
+package com.signaturelinkers.mydemolib;
+
+/**
+ * Created by Rahul Mangal on 3/26/2017.
+ */
+
+public class Rahul2 {
+    public void get2() {
+
+    }
+}
