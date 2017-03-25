1acebd1691686415359627a5a2521ad1a216f047
rahulmangal1991 <rmangal1991@gmail.com>
2017-03-26 00:20:44 +0530

    init

---
+++ mydemolib/src/main/java/com/signaturelinkers/mydemolib/Rahul.java
@@ -0,0 +1,16 @@
+package com.signaturelinkers.mydemolib;
+
+import android.util.Log;
+
+/**
+ * Created by Rahul Mangal on 3/25/2017.
+ */
+
+public class Rahul {
+
+    void getData()
+    {
+        Log.e("@@@@@","log@@@@@@@@@");
+
+    }
+}
